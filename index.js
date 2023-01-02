5e92dc4a5161a48e33c36c9c625a157a45577955
Mohsin raza <mohsin552001@gmail.com>
2023-01-02 22:28:52 +0500

    at this position  i have created delete and edit functionality and more than that

--- index.js
+++ index.js
@@ -1,24 +1,93 @@
-let express = require('express')
-const { connection } = require('./src/common/database')
-let app = express()
+let express = require("express");
+let cors = require("cors");
+const { connection } = require("./src/common/database");
+let app = express();
 
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
+
+app.post("/create", (req, res) => {
+  console.log(req.body);
+  connection.query(
+    `insert into contactsforgoogle (fullname,phone,email,website) values ('${req.body.fullname}','${req.body.phone}','${req.body.email}','${req.body.website}')`,
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        let contactId = result.insertId;
+        connection.query(
+          `select * from contactsforgoogle where id=${contactId}`,
+          (err, result) => {
+            res.send(result[0]);
+          }
+        );
+      }
+    }
+  );
+});
+
+
+//Edit contact 
+
+app.post("/update", (req, res) => {
+let id =req.body.id
+  connection.query(
+    `update contactsforgoogle set fullname='${req.body.fullname}',phone='${req.body.phone}',email='${req.body.email}',website='${req.body.website}' where id=${id}`,
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        res.status(501).send({error:err})
+      } else {
+       res.sendStatus(201)
+      }
+    }
+  );
+});
 
 
 
-app.post('/create', (req, res) => {
-    console.log(req.body)
-    connection.query(`insert into contactsforgoogle (fullname,phone,email,website) values ('${req.body.fullname}','${req.body.phone}','${req.email}','${req.website}')`, (err, result) => {
-        if (err) {
-            console.log(err)
-        } else {
-            res.send('data has been inserted successfully')
-        }
-    })
 
-})
+app.get("/contact", (req, res) => {
+  let sql = `select * from contactsforgoogle`;
+  connection.query(sql, (err, result) => {
+    if (err) {
+      return res.status(503).json({ error: err });
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+app.delete("/contact", (req, res) => {
+ 
+  let sql = `delete  from contactsforgoogle where id in (${req.body.ids.join(
+    ", "
+  )})`;
+  connection.query(sql, (err, result) => {
+    if (err) {
+      return res.status(503).json({ error: err });
+     
+    } else {
+      res.sendStatus(201);
+    }
+  });
+});
+
+
+
+app.get("/contact/:id", (req, res) => {
+    let sql = `select * from contactsforgoogle where id=${req.params.id}`;
+    connection.query(sql, (err, result) => {
+      if (err) {
+        return res.status(503).json({ error: err });
+      } else {
+        if(result.length>0) return res.send(result[0])
+        res.send(result([]))
+      }
+    });
+  });
 
 
 app.listen(3400, () => {
-    console.log('App is running on http://localhost:3400')
-})
+  console.log("App is running on http://localhost:3400");
+});
